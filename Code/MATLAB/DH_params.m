3f2f45da811ee74bb3a86c63a9f43e7fb8618c58
sswcha <seungwcha@gmail.com>
2023-11-17 13:21:31 -0800

    ahhh

--- Code/MATLAB/DH_params.m
+++ Code/MATLAB/DH_params.m
@@ -10,7 +10,9 @@ L3 = 140;           % mm
 L4 = 40.389;        % mm
 L5 = 25;            % mm  [EDIT]
 
-theta1 = 0 * pi / 180;       % rad [EDIT]
+
+% theta 2, 3, 4 are w.r.t. upward vertical axis
+theta1 =  30 * pi / 180;       % rad [EDIT]
 theta2 = -30 * pi / 180;       % rad [EDIT]
 theta3 = -30 * pi / 180;       % rad [EDIT]
 theta4 = -30 * pi / 180;       % rad [EDIT]
@@ -21,7 +23,7 @@ theta5 = -50.25 * pi / 180;       % rad
 syms theta1_sym theta2_sym theta3_sym theta4_sym theta5_sym L2_sym L3_sym L4_sym L5_sym
 
 TF_1 = TF(     0,            0,         0,   theta1_sym);
-TF_2 = TF( sym(pi)/2,        0,         0,   theta2_sym);
+TF_2 = TF( sym(pi)/2,        0,         0,   sym(pi)/2 + theta2_sym);
 TF_3 = TF(     0,          L2_sym,      0,   theta3_sym);
 TF_4 = TF(     0,          L3_sym,      0,   theta4_sym);
 TF_5 = TF(     0,          L4_sym,      0,   theta5_sym);
@@ -34,7 +36,7 @@ TF_0to6 = simplify(TF_1*TF_2*TF_3*TF_4*TF_5*TF_6);
 % convert to function
 forwardKin = matlabFunction(TF_0to6);
 
-test = forwardKin(L2,L3,L4,L5,theta1,theta2,theta3,theta4,theta5)
+PenTip = forwardKin(L2,L3,L4,L5,theta1,theta2,theta3,theta4,theta5)
 
 %% Function Definitions
 
